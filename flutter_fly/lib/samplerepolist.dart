9c91b1699dd6f64a71825b4576a5bf3229ac2a2d
Niran S Narayanan <nirans2002@gmail.com>
2021-09-06 15:19:07 +0530

    theme implementation in home page, list on progress

---
+++ flutter_fly/lib/samplerepolist.dart
@@ -0,0 +1,28 @@
+class repo {
+  final String repoName;
+  final String repodes;
+
+  repo({
+    required this.repoName,
+    required this.repodes,
+  });
+}
+
+List repolist = [
+  repo(
+    repoName: "repo 1",
+    repodes: "repo discription 1",
+  ),
+  repo(
+    repoName: "repo 2",
+    repodes: "repo discription 2",
+  ),
+  repo(
+    repoName: "repo 3",
+    repodes: "repo discription 3",
+  ),
+  repo(
+    repoName: "repo 4",
+    repodes: "repo discription 4",
+  ),
+];
